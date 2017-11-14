030eb7bd403102cf620893162341974ef602fbd2
anton <aurum0512@gmail.com>
2017-11-14 21:38:53 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sentimentant Analysis
---
+++ sentiment-analysis/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace sentimentanalysis
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // https://www.cryptocoinsnews.com/news/
+			Console.WriteLine("Hello World!");
+        }
+    }
+}
